c5fe6bfb74ac62ca5f062602215ad2f02935249d
Gabriela <gdemelo.mendes@gmail.com>
2023-10-27 19:53:12 -0300

    Created update quantity buttons component and added to cart and menu when needed

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -17,19 +17,16 @@ const cartSlice = createSlice({
       state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
     },
     increaseQuantity(state, action) {
-      const item = state.cart.find((item) => item === action.payload);
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity += 1;
       item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseQuantity(state, action) {
-      const item = state.cart.find((item) => item === action.payload);
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
       item.quantity -= 1;
       item.totalPrice = item.quantity * item.unitPrice;
 
-      if (item.quantity === 0)
-        state.cart = state.cart.filter(
-          (item) => item.pizzaId !== action.payload,
-        );
+      if (item.quantity === 0) cartSlice.caseReducers.removeItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
