584ccf3ac7e8c31f0d70adce60d459201e8ec8c8
Gabriela <gdemelo.mendes@gmail.com>
2023-10-27 19:57:45 -0300

    Updated link button styles

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 function LnkButton({ children, to }) {
   const navigate = useNavigate();
 
-  const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+  const className =
+    "text-sm text-blue-500 hover:text-blue-600 hover:underline focus:outline-none focus-visible:ring focus-visible:ring-blue-600 focus-visible:underline  focus-visible:ring-offset-2 rounded";
 
   if (to === "-1")
     return (
