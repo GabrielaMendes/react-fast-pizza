2f74134eb519505e04695c96b17c7e1c334b1272
Gabriela <gdemelo.mendes@gmail.com>
2023-10-27 19:14:47 -0300

    Added remove option to pizzas on menu that are already in cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import Button from "../../ui/Button";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
 
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
 
   function handleAddToCart() {
     const newItem = {
@@ -41,11 +44,14 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
-            <Button type="small" onClick={handleAddToCart}>
-              Add to cart
-            </Button>
-          )}
+          {!soldOut &&
+            (currentQuantity > 0 ? (
+              <DeleteItem pizzaId={id} />
+            ) : (
+              <Button type="small" onClick={handleAddToCart}>
+                Add to cart
+              </Button>
+            ))}
         </div>
       </div>
     </li>
