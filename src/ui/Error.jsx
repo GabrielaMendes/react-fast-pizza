f4ac5c17528a24f87f11bec7848b31cf9a68302e
Gabriela <gdemelo.mendes@gmail.com>
2023-10-27 19:01:26 -0300

    Styled error page

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -5,9 +5,9 @@ function Error() {
   const error = useRouteError();
 
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
+    <div className="px-4 py-6">
+      <h1 className="text-xl font-semibold mb-2">Something went wrong 😢</h1>
+      <p className="mb-7">{error.data || error.message}</p>
       <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
